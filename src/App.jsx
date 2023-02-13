61f2ede7de92836e88c13909b520ed0f94984b2d
twismdx <twism@live.com>
2023-02-13 17:21:55 +1030

    1

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,6 @@ function App() {
 				console.log(response.data)
 				console.log(link)
 				setLink(response.data)
-				console.log(response)
 				copyToClipboard()
 			})
 			.catch(function (error) {
